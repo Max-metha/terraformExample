86540b8cd50d9be07ce1a1355c32c71d12ac2106
Max Hartwig <max.hartwig@ymail.com>
2023-03-16 14:20:44 +0100

    update provider

--- provider.tf
+++ provider.tf
@@ -9,5 +9,7 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
+  tenant_id       = "4dfdfd67-3a37-4e2e-b9f0-434c7061ba33"
+  subscription_id = "ea6e6692-4d05-4c5b-9909-51c7dc5f5c2b"
   features {}
 }
